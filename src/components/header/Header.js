7ebb56d64e63c6e54c7da921016320d1ddf5070b
scavengerisland <60721508+scavengerisland@users.noreply.github.com>
2023-07-03 17:46:19 -0500

    Remove Nav Bar.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -35,80 +35,80 @@ class Header extends Component {
             <label className="menu-icon" htmlFor="menu-btn">
               <span className="navicon"></span>
             </label>
-            <ul className="menu" style={{ backgroundColor: theme.body }}>
-              <li>
-                <NavLink
-                  to="/home"
-                  tag={Link}
-                  activeStyle={{ fontWeight: "bold" }}
-                  style={{ color: theme.text }}
-                  onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                  onMouseOut={(event) => onMouseOut(event)}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/education"
-                  tag={Link}
-                  activeStyle={{ fontWeight: "bold" }}
-                  style={{ color: theme.text }}
-                  onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                  onMouseOut={(event) => onMouseOut(event)}
-                >
-                  Education
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/experience"
-                  tag={Link}
-                  activeStyle={{ fontWeight: "bold" }}
-                  style={{ color: theme.text }}
-                  onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                  onMouseOut={(event) => onMouseOut(event)}
-                >
-                  Experience
-                </NavLink>
-              </li>
-              {/*<li>*/}
-              {/*  <NavLink*/}
-              {/*    to="/projects"*/}
-              {/*    tag={Link}*/}
-              {/*    activeStyle={{ fontWeight: "bold" }}*/}
-              {/*    style={{ color: theme.text }}*/}
-              {/*    onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}*/}
-              {/*    onMouseOut={(event) => onMouseOut(event)}*/}
-              {/*  >*/}
-              {/*    Projects*/}
-              {/*  </NavLink>*/}
-              {/*</li>*/}
-              {/*<li>*/}
-              {/*  <NavLink*/}
-              {/*    to="/opensource"*/}
-              {/*    tag={Link}*/}
-              {/*    activeStyle={{ fontWeight: "bold" }}*/}
-              {/*    style={{ color: theme.text }}*/}
-              {/*    onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}*/}
-              {/*    onMouseOut={(event) => onMouseOut(event)}*/}
-              {/*  >*/}
-              {/*    Open Source*/}
-              {/*  </NavLink>*/}
-              {/*</li>*/}
-              <li>
-                <NavLink
-                  to="/contact"
-                  tag={Link}
-                  activeStyle={{ fontWeight: "bold" }}
-                  style={{ color: theme.text }}
-                  onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}
-                  onMouseOut={(event) => onMouseOut(event)}
-                >
-                  Contact Me
-                </NavLink>
-              </li>
-            </ul>
+            {/*<ul className="menu" style={{ backgroundColor: theme.body }}>*/}
+            {/*  <li>*/}
+            {/*    <NavLink*/}
+            {/*      to="/home"*/}
+            {/*      tag={Link}*/}
+            {/*      activeStyle={{ fontWeight: "bold" }}*/}
+            {/*      style={{ color: theme.text }}*/}
+            {/*      onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}*/}
+            {/*      onMouseOut={(event) => onMouseOut(event)}*/}
+            {/*    >*/}
+            {/*      Home*/}
+            {/*    </NavLink>*/}
+            {/*  </li>*/}
+            {/*  <li>*/}
+            {/*    <NavLink*/}
+            {/*      to="/education"*/}
+            {/*      tag={Link}*/}
+            {/*      activeStyle={{ fontWeight: "bold" }}*/}
+            {/*      style={{ color: theme.text }}*/}
+            {/*      onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}*/}
+            {/*      onMouseOut={(event) => onMouseOut(event)}*/}
+            {/*    >*/}
+            {/*      Education*/}
+            {/*    </NavLink>*/}
+            {/*  </li>*/}
+            {/*  <li>*/}
+            {/*    <NavLink*/}
+            {/*      to="/experience"*/}
+            {/*      tag={Link}*/}
+            {/*      activeStyle={{ fontWeight: "bold" }}*/}
+            {/*      style={{ color: theme.text }}*/}
+            {/*      onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}*/}
+            {/*      onMouseOut={(event) => onMouseOut(event)}*/}
+            {/*    >*/}
+            {/*      Experience*/}
+            {/*    </NavLink>*/}
+            {/*  </li>*/}
+            {/*  /!*<li>*!/*/}
+            {/*  /!*  <NavLink*!/*/}
+            {/*  /!*    to="/projects"*!/*/}
+            {/*  /!*    tag={Link}*!/*/}
+            {/*  /!*    activeStyle={{ fontWeight: "bold" }}*!/*/}
+            {/*  /!*    style={{ color: theme.text }}*!/*/}
+            {/*  /!*    onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}*!/*/}
+            {/*  /!*    onMouseOut={(event) => onMouseOut(event)}*!/*/}
+            {/*  /!*  >*!/*/}
+            {/*  /!*    Projects*!/*/}
+            {/*  /!*  </NavLink>*!/*/}
+            {/*  /!*</li>*!/*/}
+            {/*  /!*<li>*!/*/}
+            {/*  /!*  <NavLink*!/*/}
+            {/*  /!*    to="/opensource"*!/*/}
+            {/*  /!*    tag={Link}*!/*/}
+            {/*  /!*    activeStyle={{ fontWeight: "bold" }}*!/*/}
+            {/*  /!*    style={{ color: theme.text }}*!/*/}
+            {/*  /!*    onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}*!/*/}
+            {/*  /!*    onMouseOut={(event) => onMouseOut(event)}*!/*/}
+            {/*  /!*  >*!/*/}
+            {/*  /!*    Open Source*!/*/}
+            {/*  /!*  </NavLink>*!/*/}
+            {/*  /!*</li>*!/*/}
+            {/*  <li>*/}
+            {/*    <NavLink*/}
+            {/*      to="/contact"*/}
+            {/*      tag={Link}*/}
+            {/*      activeStyle={{ fontWeight: "bold" }}*/}
+            {/*      style={{ color: theme.text }}*/}
+            {/*      onMouseEnter={(event) => onMouseEnter(event, theme.highlight)}*/}
+            {/*      onMouseOut={(event) => onMouseOut(event)}*/}
+            {/*    >*/}
+            {/*      Contact Me*/}
+            {/*    </NavLink>*/}
+            {/*  </li>*/}
+            {/*</ul>*/}
           </header>
         </div>
       </Fade>
